67c18208dfccc9f58015f2c981def18c50c140e8
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2020-04-21 01:26:01 +0100

    Update cutscene

--- core/src/ldjam/hamorigami/cutscene/IntroCutscene.java
+++ core/src/ldjam/hamorigami/cutscene/IntroCutscene.java
@@ -3,6 +3,7 @@ package ldjam.hamorigami.cutscene;
 import aurelienribon.tweenengine.BaseTween;
 import aurelienribon.tweenengine.Tween;
 import aurelienribon.tweenengine.TweenCallback;
+import aurelienribon.tweenengine.TweenEquations;
 import de.bitbrain.braingdx.context.GameContext2D;
 import de.bitbrain.braingdx.tweens.GameObjectTween;
 import de.bitbrain.braingdx.tweens.SharedTweenManager;
@@ -16,24 +17,23 @@ public class IntroCutscene implements CutsceneSetup {
 
    @Override
    public void cleanup(GameContext2D context) {
-
+      SharedTweenManager.getInstance().killTarget(kodama);
    }
 
    @Override
    public void setup(GameContext2D context) {
       EntityFactory entityFactory = new EntityFactory(context);
-      kodama = entityFactory.spawnSpirit(SpiritType.SPIRIT_EARTH, context.getGameCamera().getScaledCameraWidth() / 4f, 0f);
+      kodama = entityFactory.spawnSpirit(SpiritType.SPIRIT_EARTH, context.getGameCamera().getScaledCameraWidth() / 3f, 0f);
       kodama.setDimensions(64f, 64f);
       kodama.getColor().a = 0f;
       float currentX = kodama.getLeft();
       Tween.to(kodama, GameObjectTween.ALPHA, 4f).delay(6f)
             .target(1f)
             .start(SharedTweenManager.getInstance());
-      Tween.call(new TweenCallback() {
-         @Override
-         public void onEvent(int type, BaseTween<?> source) {
-
-         }
-      }).delay(10f).start(SharedTweenManager.getInstance());
+      Tween.to(kodama, GameObjectTween.POS_X, 4f).delay(9f)
+            .target(currentX - 200f)
+            .repeatYoyo(Tween.INFINITY, 0f)
+            .ease(TweenEquations.easeInOutCubic)
+            .start(SharedTweenManager.getInstance());
    }
 }
